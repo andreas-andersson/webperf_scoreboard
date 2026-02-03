172b34095e7d3dc1c4079ade0b87c08d155db58a
Andreas Andersson <andreas@aikon.se>
2026-02-03 16:58:44 +0100

    feature: enhance search input with focus and blur handling

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,7 +2,7 @@
 
 import { SearchIcon } from "lucide-react";
 import { InputGroup, InputGroupAddon, InputGroupInput } from "./ui/input-group";
-import { RefObject } from "react";
+import { RefObject, useEffect, useRef } from "react";
 
 interface SearchProps {
   tableBodyRef?: RefObject<HTMLTableSectionElement | null>;
@@ -10,6 +10,13 @@ interface SearchProps {
 
 export function Search({ tableBodyRef }: SearchProps) {
 
+  const searchRef = useRef<HTMLInputElement>(null);
+
+  /**
+   * Handle filtering table rows based on search input
+   * @param e Change event from the search input
+   * @returns void
+   */
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     const term = e.target.value.toLowerCase();
     if (!tableBodyRef?.current) return;
@@ -26,9 +33,32 @@ export function Search({ tableBodyRef }: SearchProps) {
     }
   }
 
+  /**
+   * Handle keydown events for focusing and blurring the search input
+   * @param e Keyboard event
+   * @returns void
+   */
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "/" && !e.repeat) {
+      e.preventDefault();
+      searchRef.current?.focus();
+    }
+
+    if (e.key === "Escape" && document.activeElement === searchRef.current) {
+      searchRef.current?.blur();
+    }
+  }
+
+  // Set up keydown event listener
+  useEffect(() => {
+    // Focus search input on "/" key press
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, []);
+
   return (
     <InputGroup className="mb-4 w-full border-none">
-      <InputGroupInput accessKey="s" placeholder="Search..." onChange={handleSearch} />
+      <InputGroupInput ref={searchRef} accessKey="s" placeholder="Search..." onChange={handleSearch} />
       <InputGroupAddon>
       <SearchIcon />
       </InputGroupAddon>
