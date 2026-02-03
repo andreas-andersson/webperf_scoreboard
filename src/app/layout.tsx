1812c46d9df232fa03ce7e679fcf3608a21f4152
Andreas Andersson <andreas@aikon.se>
2026-02-03 12:50:25 +0100

    feature: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/next"
 
 const mono = JetBrains_Mono({
   subsets: ["latin"],
@@ -31,6 +32,7 @@ export default function RootLayout({
           >
             {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
