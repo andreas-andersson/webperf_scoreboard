6abd859fd2669d16faf6298ef259f71c0eec0269
Andreas Andersson <andreas@aikon.se>
2026-02-11 11:03:38 +0100

    feature: update page title and description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
+import { sql } from "drizzle-orm";
+import { ArrowUpRight } from "lucide-react";
 import { useReplica } from "@/db";
 import { sites, scans } from "@/db/schema";
-import { sql } from "drizzle-orm";
 import { Leaderboard } from "@/components/Leaderboard";
 import {
   Card,
@@ -81,9 +82,17 @@ export default async function Home() {
 
   return (
     <main className="container mx-auto py-10 px-4">
-      <h1 className="text-4xl font-bold mb-6 font-mono tracking-tight uppercase">
-        WEBPERF SCOREBOARD
-      </h1>
+      <div className="flex flex-col sm:flex-row sm:items-end mb-6 gap-4">
+        <div className="header">
+            <span className="text-xs text-muted-foreground uppercase tracking-wide font-mono">
+              Webperf: <a href="https://webperxf.se/category/kommuner/" target="_blank" className="hover:text-primary transition-colors inline-flex flex-row">Municipalities <ArrowUpRight size={16} /></a>
+            </span>
+            <h1 className="text-3xl md:text-4xl font-bold mb-0 font-mono tracking-tight uppercase w-100 sm:w-min lg:w-max">
+              Municipality scoreboard
+            </h1>
+        </div>
+        <p className="hidden sm:block text-xs text-muted-foreground sm:pl-4">A ranking of municipalities based on their web performance scores, with historical comparisons.</p>
+      </div>
       <Card className="border-border">
         <CardContent>
           <Leaderboard leaderboard={leaderboard} />
