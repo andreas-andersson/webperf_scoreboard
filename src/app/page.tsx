3cb55bff2b71bfbfc2f39781781f6c8e78de3992
Andreas Andersson <andreas@aikon.se>
2026-02-11 11:04:52 +0100

    fix: link url spelling mistake

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -85,7 +85,7 @@ export default async function Home() {
       <div className="flex flex-col sm:flex-row sm:items-end mb-6 gap-4">
         <div className="header">
             <span className="text-xs text-muted-foreground uppercase tracking-wide font-mono">
-              Webperf: <a href="https://webperxf.se/category/kommuner/" target="_blank" className="hover:text-primary transition-colors inline-flex flex-row">Municipalities <ArrowUpRight size={16} /></a>
+              Webperf: <a href="https://webperf.se/category/kommuner/" target="_blank" className="hover:text-primary transition-colors inline-flex flex-row">Municipalities <ArrowUpRight size={16} /></a>
             </span>
             <h1 className="text-3xl md:text-4xl font-bold mb-0 font-mono tracking-tight uppercase w-100 sm:w-min lg:w-max">
               Municipality scoreboard
