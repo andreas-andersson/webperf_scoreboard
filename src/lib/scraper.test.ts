9631ba912cbdd14f3fa4f77f8ee16b0f42735468
Andreas Andersson <andreas@aikon.se>
2026-04-08 09:00:04 +0200

    feature: organize test file

--- src/lib/scraper.test.ts
+++ src/lib/scraper.test.ts
@@ -5,7 +5,7 @@
  */
 
 import { describe, it, expect } from 'vitest'
-import { scrapeScoreboard, scrapeSiteDetails } from '../scraper'
+import { scrapeScoreboard, scrapeSiteDetails } from './scraper'
 
 /**
  *  Test a string to see if it is a valid URL
